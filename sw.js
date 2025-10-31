b0369195999e7da14591cbd765560a9a2c77595f
Shoki08 <yamakou1623@gmail.com>
2025-11-01 00:22:46 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,11 +1,11 @@
 const CACHE_NAME = 'music-release-manager-v1';
 const urlsToCache = [
-  '/',
-  '/index.html',
-  '/app.js',
-  '/manifest.json',
-  '/icon-192.png',
-  '/icon-512.png'
+  './',
+  './index.html',
+  './app.js',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
 ];
 
 // インストール時
@@ -14,10 +14,7 @@ self.addEventListener('install', event => {
     caches.open(CACHE_NAME)
       .then(cache => {
         console.log('Opened cache');
-        return cache.addAll(urlsToCache.map(url => {
-          // GitHub Pagesの場合、リポジトリ名を考慮
-          return new Request(url, { cache: 'reload' });
-        })).catch(err => {
+        return cache.addAll(urlsToCache).catch(err => {
           console.log('Cache addAll error:', err);
         });
       })
@@ -47,18 +44,15 @@ self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
       .then(response => {
-        // キャッシュがあればそれを返す
         if (response) {
           return response;
         }
 
         return fetch(event.request).then(response => {
-          // 有効なレスポンスでない場合はそのまま返す
           if (!response || response.status !== 200 || response.type !== 'basic') {
             return response;
           }
 
-          // レスポンスをクローンしてキャッシュに保存
           const responseToCache = response.clone();
           caches.open(CACHE_NAME)
             .then(cache => {
@@ -67,8 +61,7 @@ self.addEventListener('fetch', event => {
 
           return response;
         }).catch(() => {
-          // オフライン時の処理
-          return caches.match('/index.html');
+          return caches.match('./index.html');
         });
       })
   );
